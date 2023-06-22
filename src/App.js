23209250c2dfe1479a060fa7355e12b1abf86401
Hongen Lei <hongenlei99@gmail.com>
2023-06-21 23:15:39 -0700

    Fix routing problem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Mainpage from "./pages/home/Mainpage";
 import Projectpage from './pages/projects/Projectpage';
 
@@ -10,16 +10,14 @@ import Projectpage from './pages/projects/Projectpage';
 const App = () => {
   return (
     <>
-    <BrowserRouter>
       <Routes>
         <Route path = "/" element = {<Mainpage />}/>
         <Route path = "/projects" element = {<Projectpage />}/>
       </Routes>
-
-    </BrowserRouter>
-
     </>
   )
 }
 
 export default App
+
+
